a34f04841a1c8388713a17171c84aa9bf847358b
mlafave <matt.lafave@gmail.com>
2014-03-07 14:20:44 -0500

    Added examples and readme, changed name and location of driver script.

--- perl/rcFastq.pl
+++ perl/rcFastq.pl
@@ -1,27 +1,21 @@
 #!/usr/bin/perl
-use 5.8.8; # Added by Matt LaFave
-
-# AUTHOR: Joseph Fass
-# LAST REVISED: September 2010
-# 
-# The Bioinformatics Core at UC Davis Genome Center
-# http://bioinformatics.ucdavis.edu
-# Copyright (c) 2008 The Regents of University of California, Davis Campus.
-# All rights reserved.
-
-# USAGE: cat sequence.fastq | ./rcFastq.pl > sequence.rc.fastq
-
+use 5.8.8;
 
 while (<>) {
 	
-	$h1 = $_;
-	chomp ($s = <>);
-	$h2 = <>;
-	chomp ($q = <>);
-	$s = reverse $s;
-	$s =~ tr/ATCGNatcgn/TAGCNtagcn/;
-	$q = reverse $q;
-	print $h1.$s."\n".$h2.$q."\n";
-} # while
+	chomp;
+	$name = $_;
+	chomp ($seq = <>);
+	chomp ($plus = <>);
+	chomp ($qual = <>);
+	
+	$seq = reverse $seq;
+	$seq =~ tr/ACGTNacgtn/TGCANtgcan/;
+	
+	$qual = reverse $qual;
+	
+	print $name."\n".$seq."\n".$plus."\n".$qual."\n";
+	
+}
 
 exit;
